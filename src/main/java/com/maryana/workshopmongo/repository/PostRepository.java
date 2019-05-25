e17654ed4c3e55a66876737efc5d2a1ce5303557
Maryana Marinho <maryanalirius@hotmail.com>
2019-05-25 20:44:14 -0300

    Consulta simples com @Query

--- src/main/java/com/maryana/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/maryana/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.maryana.workshopmongo.repository;
 import com.maryana.workshopmongo.domain.Post;
 import com.maryana.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,10 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    //busca usando o metodo do spring
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    //busca usando consulta em mongo
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 }
